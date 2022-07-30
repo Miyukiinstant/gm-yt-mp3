92b90a6b06f4edf738cadddaec57131daa345b25
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-07-31 00:08:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -77,8 +77,7 @@
     $host = 'https://getn.topsandtees.space';
     $root = $host."/s/".explode("https://getn.topsandtees.space/",explode('"',file_get_contents($root))[55])[1]; //Fetches the main root link for furhter requests 
     $yt = $_GET['yt'];
-    $yt = explode("&list",$yt)[0];
-    $yt = explode("?list",$yt)[0];
+    $yt = explode("list=",$yt)[0];
     $data = array(
         'q' => $yt
     );
