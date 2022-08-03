17e77ba29893f48a3a8b68dd874aa3163491d498
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-08-03 21:13:45 +0200

    Update error.php

--- api/tf2tts/error.php
+++ api/tf2tts/error.php
@@ -2,7 +2,7 @@
     require_once "../../pages/style.php";
 ?>
 <div class=card>
-    <h4><?php echo $_SERVER['SERVER_NAME']."?author=GLaDOS&text=hello" ?></h4>
+    <h4><?php echo $_SERVER['SERVER_NAME']."?voice=GLaDOS&text=hello" ?></h4>
     <ul>
         <li>voice</li>
         <li>text</li>
