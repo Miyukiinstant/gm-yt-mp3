039ec859030c350bdddcf3c60200800878f20980
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-08-03 02:23:47 +0200

    Update index.php

--- api/googletts/index.php
+++ api/googletts/index.php
@@ -3,8 +3,6 @@
 if(!isset($_GET['voice']) or !isset($_GET['q'])){
     return;
 }
-?>
-<audio controls autoplay name="media">
-    <source src="https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=<?php echo $_GET['voice']."&q=".$_GET['q'];?>" type="audio/mpeg">
+header("Location:https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl={$_GET['voice']}&q={$_GET['q']}");
 
-</audio>
+?>
